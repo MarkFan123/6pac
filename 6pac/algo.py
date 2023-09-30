828d72b53d002fdeedc7dbae33f1d1af6addf62e
MarkFan123 <146504560+MarkFan123@users.noreply.github.com>
2023-09-30 12:46:07 -0400

    Update algo.py

--- 6pac/algo.py
+++ 6pac/algo.py
@@ -1,103 +1,203 @@
 # You can modify this file to implement your own algorithm
 
 from constants import *
-
-
-
-def check(grid):
-    for i in grid:
-        for j in i:
-            if(j == 'c'):
-                return 1
-    return 2
 """
 # o = normal pellet, e = empty space, O = power pellet, c = cherry position
 # I = wall, n = ghost chambers
 """
-def turndetect(grid):
-    t=0
-    turns=[]
-    for i in range(len(grid)-1):
-        for j in range(len(grid[0])-1):
-            if(grid[i][j] == o):
-                if(grid[i+1][j] == o):
-                    t+=1
-                if(grid[i-1][j] == o):
-                    t+=1
-                if(grid[i][j+1] == o):
-                    t+=1
-                if(grid[i][j-1] == o):
-                    t+=1
-            if( t>=3):
-                turns.append((i,j))
-            t=0
-    return turns
-
-class TreeNode:
-    def __init__(self, data=None, left=None, right=None, up=None, down=None):
-        self.data = data  # 数据域的值
-        self.left = left  # 左孩子的指针
-        self.right = right  # 右孩子的指针
-        self.up = up
-        self.down = down
-
-class Tree:
-    def __init__(self, root=None):
-        self.root = root  # 二叉树的根节点
-
-def path(start, end):
-    pathtree = Tree()
-    pathtree.root = TreeNode()
-    pathtree.root.data = start
-    root = pathtree.root
-    node = pathtree.root
-    iterations = 0
-    plan(node, root, end, iterations)
+#attempt to use tree to calculate route, failed.
+# #
+# class TreeNode:
+#     def __init__(self, data=None, left=None, right=None, up=None, down=None):
+#         self.data = data  # 数据域的值
+#         self.left = left  # 左孩子的指针
+#         self.right = right  # 右孩子的指针
+#         self.up = up
+#         self.down = down
 
 
-def plan(map, node, root, end, iteration):
-    iteration += 1
-    if iteration > 5:
-        return
-    if node.data[0] == end[0] and node.data[1] == end[1]:
-        return
-    if map[node.data[0]+1][node.data[1]] == 0:
-        node.left = TreeNode()
-        node.left.data = [node.data[0]+1, node.data[1]]
-        node = node.left
-        plan(map,node, root, end, iteration)
+# class Tree:
+#     def __init__(self, root=None):
+#         self.root = root  # 二叉树的根节点
 
-    if map[node.data[0]-1][node.data[1]] == 0:
-        node.right = TreeNode()
-        node.right.data = [node.data[0]-1, node.data[1]]
-        node = node.right
-        plan(map,node, root, end, iteration)
+# # def shortestway(root):
+# #     node=root
+# #     way=[]
+# #     length=0
+# #     while(node != None):
+# #         if
 
-    if map[node.data[0]][node.data[1]+1] == 0:
-        node.up = TreeNode()
-        node.up.data = [node.data[0], node.data[1]+1]
-        node = node.up
-        plan(map,node, root, end, iteration)
 
-    if map[node.data[0]][node.data[1]-1] == 0:
-        node.down = TreeNode()
-        node.down.data = [node.data[0], node.data[1]-1]
-        node = node.down
-        plan(map,node, root, end, iteration)
-    return
+# def printtree(trial, i):
+#     i += 1
+#     pathtree = trial
+#     #print(pathtree.data, '->')
+#     if pathtree.left != None:
+#         printtree(pathtree.left, i)
+#     if pathtree.right != None:
+#         printtree(pathtree.right, i)
+#     if pathtree.up != None:
+#         printtree(pathtree.up, i)
+#     if pathtree.down != None:
+#         printtree(pathtree.down, i)
+#     return pathtree.data
+
+
+# def path(map,start, end):
+#     pathtree = Tree()
+#     pathtree.root = TreeNode()
+#     pathtree.root.data = start
+#     root = pathtree.root
+#     node = pathtree.root
+#     iterations = 0
+#     i = 0
+#     if(plan(map, node, root, end, iterations) ==1 ):
+#         return printtree(pathtree.root, i)
+        
     
     
 
-def get_next_coordinate(grid, location):
-    print(turndetect(grid))
-    
-    return location
+
+# def plan(map, node, root, end, iteration):
+#     iteration += 1
+#     if iteration > 20:
+#         return
+#     if map[node.data[0]+1][node.data[1]]==end:
+#         node.left = TreeNode()
+#         node.left.data = [node.data[0]+1, node.data[1]]
+#         return 1
+#     if map[node.data[0]-1][node.data[1]]==end:
+#         node.left = TreeNode()
+#         node.left.data = [node.data[0]-1, node.data[1]]
+#         return 1
+#     if map[node.data[0]][node.data[1]+1]==end:
+#         node.left = TreeNode()
+#         node.left.data = [node.data[0]+1, node.data[1]]
+#         return 1
+#     if map[node.data[0]][node.data[1]-1]==end:
+#         node.left = TreeNode()
+#         node.left.data = [node.data[0]+1, node.data[1]]
+#         return 1
+#     if map[node.data[0]+1][node.data[1]] ==e :
+#         node.left = TreeNode()
+#         node.left.data = [node.data[0]+1, node.data[1]]
+#         map[node.data[0]][node.data[1]] = 9
+#         if map[node.data[0]+1][node.data[1]]==end:
+#             return
+#         node = node.left
+#         plan(map, node, root, end, iteration)
+ 
+
+#     if map[node.data[0]-1][node.data[1]] == e:
+#         node.right = TreeNode()
+#         node.right.data = [node.data[0]-1, node.data[1]]
+#         map[node.data[0]][node.data[1]] = 9
+#         if map[node.data[0]-1][node.data[1]]==end:
+#             return
+#         node = node.right
+#         plan(map, node, root, end, iteration)
+
+#     if map[node.data[0]][node.data[1]+1] == e:
+#         node.up = TreeNode()
+#         node.up.data = [node.data[0], node.data[1]+1]
+#         map[node.data[0]][node.data[1]] = 9
+#         if map[node.data[0]][node.data[1]+1]==end:
+#             return
+#         node = node.up
+#         plan(map, node, root, end, iteration)
+
+#     if map[node.data[0]][node.data[1]-1] == e:
+#         node.down = TreeNode()
+#         node.down.data = [node.data[0], node.data[1]-1]
+#         map[node.data[0]][node.data[1]] = 9
+#         if map[node.data[0]][node.data[1]-1]==end:
+#             return
+#         node = node.down
+#         plan(map, node, root, end, iteration)
+#     return
+
+##another failed method
+def distance(map, location):
+    x=location[0]
+    y=location[1]
+    mini=3000
+    mini2=300
+    for i in range(len(map)):
+        for j in range(len(map[0])):
+            if(map[i][j] == o):
+                if(abs(i-x)<mini and i!=x):
+                    mini=abs(i-x)
+                    deltax=i-x
+                if(abs(j-y)<mini2 and j!=y):
+                    mini2=abs(j-y)
+                    deltay=j-y
+                    
+    deltax=int(deltax/abs(deltax))
+    deltay=int(deltay/abs(deltay))
+    return (deltax, deltay)
 
 def close(map, location):
     go=[]
     x=location[0]
     y=location[1]
-    for j in range(1,1000):
+    deltax,deltay=distance(map, location)
+    
+    if map[x+deltax][y] == o:
+        go=(x+deltax,y)
+        return go
+    if map[x][y+deltay] == o:
+        go=(x,y+deltay)
+        return go
+    if map[x+deltax][y] == o:
+        go=(x+deltax,y)
+        return go
+    if map[x][y+deltay] == o:
+        go=(x,y+deltay)
+        return go
+    if map[x+1][y] == o:
+        go=(x+1,y)
+        return go
+    if map[x-1][y] == o:
+        go=(x-1,y)
+        return go
+    if map[x][y+1] == o:
+        go=(x,y+1)
+        return go
+    if map[x][y-1] == o:
+        go=(x,y-1)
+        return go
+    map[x][y]==100
+    d1,d2=distance(map, (x+deltax,y+deltay))
+    # if d1!=(-deltax) :       
+    #     if map[x+deltax][y] == e:
+    #         go=(x+deltax,y)
+    #         return go
+    # if d2!=(-deltay) :
+    #     if map[x][y+deltay] == e:
+    #         go=(x,y+deltay)
+    #         return go
+    map[x][y]==100
+    if map[x+1][y] == e:
+        go=(x+1,y)
+        map[x+1][y]==100
+        return go
+    if map[x-1][y] == e:
+        go=(x-1,y)
+        map[x-1][y]==100
+        return go
+    if map[x][y+1] == e:
+        go=(x,y+1)
+        map[x][y+1]==100
+        return go
+    if map[x][y-1] == e :
+        go=(x,y-1)    
+        map[x][y-1] == 100
+        return go
+def pather(map, location):
+    go=[]
+    x=location[0]
+    y=location[1]
+    while(1):
         
         if(map[x+1][y] == o):
             go.append((x+1,y))
@@ -130,12 +230,11 @@ def close(map, location):
             go.append((x,y))
             continue
     return go
-
-
-
+###if I can give out a route as a array rather than just points would be good, 
+##This method genrates a route between two o points, they are not the shortest route
 def get_next_coordinate(grid, location):
     map=grid
-    way=close(map, location)
-
-    return way 
+    go=pather(map, location)
+    
+    return go[0] 
 
